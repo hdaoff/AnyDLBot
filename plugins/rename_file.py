a10a9d4829728c66b8c2a8ef3b272c0b100dee39
hdaoff <58559310+hdaoff@users.noreply.github.com>
2019-12-23 14:33:42 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -172,7 +172,7 @@ async def rename_doc(bot, update):
             media_album_p = []
             if images is not None:
                 i = 0
-                caption = "© @AnyDLBot"
+                caption = "Screenshot By @hdarena | https://hdarena1.cf/"
                 for image in images:
                     if os.path.exists(image):
                         if i == 0:
